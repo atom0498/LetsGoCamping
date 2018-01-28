1d20753ccb937f1982dbf00fd089e89562fce397
Deepak Yadav <deepakyadav1967@gmail.com>
2018-01-28 14:43:24 +0000

    added hard image

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var commentRoutes=require("./routes/comments"),
     campgroundRoutes=require("./routes/campgrounds"),
     indexRoutes      =require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp");
-//mongoose.connect("mongodb://yelp_camp:yelpcamp@ds011314.mlab.com:11314/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://yelp_camp:yelpcamp@ds011314.mlab.com:11314/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
